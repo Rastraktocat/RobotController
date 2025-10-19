06d2f4e15822aeb77866f2e580888cec408a2251
Adi Deshpande <adindeshpande@gmail.com>
2025-10-19 13:10:45 -0500

    This is basic motor controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller.java
@@ -1,28 +1,34 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-@Autonomous(name = "motor1")
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.mechanisms.MotorPack;
+import org.firstinspires.ftc.teamcode.mechanisms.TouchSensorPack;
+
+@TeleOp
 public class Controller extends OpMode{
     // Go into configure robot on the driver hub.
     // Click new and connect to the control hub.
     // Go to motors and connect it to the right port.
 
 
-    // Plug a controller into the driver hub to connect the controller.
-    DcMotor motor;
+    // Plug a controller into the dr
+    MotorPack mp = new MotorPack();
+    TouchSensorPack tsp = new TouchSensorPack();
     @Override
     public void init(){
-        motor = hardwareMap.get(DcMotor.class, "motor1");
+        mp.init(hardwareMap);
+        tsp.init(hardwareMap);
         // If we named the motor motor1 this would work.
     }
     @Override
     public void loop(){
-        motor.setPower(1); // This is testing code. The motor should run
-        // when this happens.
-
+        if (tsp.isTouchSensorPressed()){
+            mp.setMotorSpeed(1.0); // This is testing code. The motor should run
+            // when this happens.
+        }
         // motor.setPower(gamepad1.left_trigger);
-        // This assigns a being powered to left trigger.
+        // This assigns a beings powered to left trigger.
 
 
     }
