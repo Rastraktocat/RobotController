3ce8349e0d751d19a926aa3c8605dd7e74dc1034
samarthwashere <samjapan2078@gmail.com>
2025-10-30 20:43:46 -0500

    finished mecanum drivetrain code w/ field orientation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MecanumDrive.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 public class MecanumDrive
 
 {
@@ -18,9 +20,43 @@ public class MecanumDrive
         double backRightPower = forward + strafe - rotate;
 
         double maxPower = 1.0;
+
+        /* the maxSpeed variable is unnecessary for comp's, but can be helpful during outreach if
+        we're assisting young kids and the robot might be too fast
+        double maxSpeed = 0.3; */
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        frontLeftMotor.setPower(frontLeftPower / maxPower);
+        backLeftMotor.setPower(backLeftPower / maxPower);
+        frontRightMotor.setPower(frontRightPower / maxPower);
+        backRightMotor.setPower(backRightPower / maxPower);
+
         // resume here: https://youtu.be/sFCO4du5IZk?list=PLRHdgFNRLyaPiZ5rvINwMmGMHEIL9usla&t=970
     }
+
+    public void driveFieldRelative(double forward, double strafe, double rotate) {
+        double theta = Math.atan2(forward, strafe);
+        double r = Math.hypot(strafe, forward);
+
+        theta = AngleUnit.normalizeRadians(theta - imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        double newForward = r * Math.sin(theta);
+        double newStrafe = r * Math.cos(theta);
+
+        this.drive(newForward, newStrafe, rotate);
+    }
+
     public void init(HardwareMap hwMap) {
+        /*
+        front_left_motor - port 0
+        back_left_motor - port 1
+        front_right_motor - port 2
+        back_right_motor - port 3
+         */
         frontLeftMotor = hwMap.get(DcMotor.class, "front_left_motor");
         backLeftMotor = hwMap.get(DcMotor.class, "back_left_motor");
         frontRightMotor = hwMap.get(DcMotor.class, "front_right_motor");
@@ -44,11 +80,11 @@ public class MecanumDrive
 
         // for field orientation
         RevHubOrientationOnRobot revOrientation = new RevHubOrientationOnRobot(
-                // assume looking at a robot from a top-down view
-                // with its 'front' at the top
-                // and its 'back' at the bottom
-                // 'up' refers to something facing towards you
-                // while 'down' refers to something facing the ground
+                /* assume looking at a robot from a top-down view
+                with its 'front' at the top
+                and its 'back' at the bottom
+                'up' refers to something facing towards you
+                while 'down' refers to something facing the ground */
                 RevHubOrientationOnRobot.LogoFacingDirection.UP, // 'REV' logo on control hub is facing towards you
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD // USB-C port is closer to the top
         );
